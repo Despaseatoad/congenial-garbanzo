b5a8f17a4b657bfff2e0035f59faddaaf75d6e7a
Despaseatoad <hsjimson@protonmail.com>
2019-07-16 21:02:54 +0200

    Add check if ran as root to install.sh and uninstall.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
-#!/bin/sh 
-mkdir /usr/local/share/cuckoo
-cp cuckoo.ogg LICENSE README.md /usr/local/share/cuckoo/
-cp cuckoo /usr/local/bin/
+#!/bin/bash
+if ((EUID))
+    then
+        echo "Please run this as root."
+    else
+        mkdir /usr/local/share/cuckoo
+        cp cuckoo.ogg LICENSE README.md /usr/local/share/cuckoo/
+        cp cuckoo /usr/local/bin/
+fi
--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,8 @@
-#!/bin/sh 
-rm -rf /usr/local/share/cuckoo
-rm -f /usr/local/bin/cuckoo
+#!/bin/bash
+if ((EUID))
+    then
+        echo "Please run this as root."
+    else        
+        rm -rf /usr/local/share/cuckoo
+        rm -f /usr/local/bin/cuckoo
+fi
